fcb664b063d7c2508ea0ab1eb46ffccbdab6d568
EGMJ <gabrieledson730@gmail.com>
2024-11-11 07:47:49 -0300

    feat: add bd e telas

---
+++ app/src/main/java/com/example/events_check_in/model/Cliente.java
@@ -0,0 +1,47 @@
+package com.example.events_check_in.model;
+
+public class Cliente {
+    private Integer id;
+    private String nome,cpf,telefone;
+
+    public Cliente(){}
+
+    public Cliente(Integer id, String nome, String cpf, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
